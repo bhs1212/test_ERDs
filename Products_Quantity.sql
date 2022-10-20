6fbd8a693910db5964e3d38772822718f9fe798a
bhs1212 <hasun17kh@gmail.com>
2022-10-20 13:58:28 +0900

    hasun

--- Products_Quantity.sql
+++ Products_Quantity.sql
@@ -1,8 +1,7 @@
 -- 제품명, 가격, 주문 개수 표시
-SELECT Products.ProductName, Products.Price, COUNT(OrderDetails.Quantity)
+SELECT Products.ProductName, Products.Price, OrderDetails.Quantity
 FROM Products
 INNER JOIN OrderDetails
 ON Products.ProductID = OrderDetails.ProductID
-GROUP BY Products.ProductName, Products.Price
 ;
--- 77
+-- 2155
